bfe20f4745cef75b1600e0cb88d03a8806467b97
Nabhahn Kamaal <NabhahnK@gmail.com>
2022-07-23 10:25:57 -0700

    added route to books

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.get("/books", (req, res) => {
+    res.json("some books");
+});
 
 app.listen(PORT, () => {
     console.log(`server runing on port: ${PORT}`);
